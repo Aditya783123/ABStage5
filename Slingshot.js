aef337cba835ecf8d11f8a5977abaa904235f95c
Aditya783123 <1051984@etusd.org>
2020-11-17 18:42:37 -0800

    Aditya - created catapult and rubber band

--- Slingshot.js
+++ Slingshot.js
@@ -6,7 +6,12 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+
+        //loading the 3 images of slingshot, 2 of which are catapult pictures, and the third one is the rectangular patch
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +21,31 @@ class SlingShot{
     }
 
     display(){
+
+        //displaying the catapult img's
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+           
+            stroke(48, 22, 8);
+
+            //if the bird is behind the catapult, then keep position of rectangular patch and rubber band different
+            if(pointA.x < 220){
+                strokeWeight(7);                
+                line(pointA.x - 20, pointA.y, pointB.x - 10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3, pointA.x - 30, pointA.y - 10, 15, 30);
+            }
+            
+            //if not, keep it the same
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x - 10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3, pointA.x + 25, pointA.y - 10, 15, 30);
+            }
         }
     }
     
